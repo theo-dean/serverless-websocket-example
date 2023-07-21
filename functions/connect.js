37bfa0f211c8fb4bbd3716eb78047aace2a4526e
Theo Dean <39389891+theo-dean@users.noreply.github.com>
2023-07-21 12:07:01 +0100

    sendMessage

--- functions/connect.js
+++ functions/connect.js
@@ -1,19 +1,29 @@
 const dynamodb = require("@aws-sdk/client-dynamodb");
 
+const client = new dynamodb.DynamoDBClient({region: "eu-west-1"});
+
 module.exports.handler = async (event) => {
     const connectionId = event.requestContext.connectionId;
     const domainName = event.requestContext.domainName;
     const stageName = event.requestContext.stage;
-    const qs = event.queryStringParameters
-    console.log('Connection ID: ', connectionId, 'Domain Name: ', domainName, 'Stage Name: ', stageName, 'Query Strings: ', qs)
+    const qs = event.queryStringParameters;
+    console.log('Connection ID: ', connectionId, 'Domain Name: ', domainName, 'Stage Name: ', stageName, 'Query Strings: ', qs);
 
     try {
-        const client = new dynamodb.DynamoDBClient({region: "eu-west-1"});
+        //if (!requestId) throw new Error("Invalid RequestId", requestId);
+        const requestId = qs["requestId"];
+        console.log("ReqId?", requestId);       
         await client.send(new dynamodb.PutItemCommand({
             TableName: "ConnectionsTable",
             Item: {
                 "connectionId": {
-                    "S": connectionId
+                    "S": requestId
+                },
+                "products": {
+                    "M": {}
+                },
+                "ttl": {
+                    "N": getTtl(30)
                 }
             }
         }));
@@ -24,4 +34,8 @@ module.exports.handler = async (event) => {
     return {"statusCode": 200, body: JSON.stringify({
         connectionId
     })}; // Can the client access this data?
+}
+
+const getTtl = (minutes) => {
+    return parseInt((Date.now() / 1000) + minutes*60);
 }
